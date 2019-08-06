ce6ca0c631e4bfefa0ae7910b2f520a0262876f3
Alexandre Mulatinho <alex@mulatinho.net>
2019-08-06 18:19:24 -0300

    updating initialization

--- src/main.c
+++ src/main.c
@@ -1,21 +1,23 @@
-//		main.c
-//		
-//		Copyright 2010 Alexandre Mulatinho <alex@mulatinho.net>
-//		
-//		This program is free software; you can redistribute it and/or modify
-//		it under the terms of the GNU General Public License as published by
-//		the Free Software Foundation; either version 2 of the License, or
-//		(at your option) any later version.
-//		
-//		This program is distributed in the hope that it will be useful,
-//		but WITHOUT ANY WARRANTY; without even the implied warranty of
-//		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-//		GNU General Public License for more details.
-//		
-//		You should have received a copy of the GNU General Public License
-//		along with this program; if not, write to the Free Software
-//		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//		MA 02110-1301, USA.
+/*
+ * main.c
+ *
+ * Copyright 2010 Alexandre Mulatinho <alex@mulatinho.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *		MA 02110-1301, USA.
+ */
 
 #include "main.h"
 
@@ -39,29 +41,33 @@ int main(int argc, char **argv)
 int main(int argc, char **argv)
 {
 	int opt = 0;
-	int user = 0;
 	char *env_pwd = getenv("PWD");
 
 	sfm_set_current_path(env_pwd);
 
+	if (argc <= 1) {
+		sfm_gui();
+		return 0;
+	}
 	while ((opt = getopt(argc, argv, "gnv")) != -1) {
 		switch (opt) {
-		case 'g':
-			sfm_gui();
+		case 'u':
+		case 'h':
+			// user and hostname, future.
 			break;
 		case 'n':
 			sfm_ncurses();
 			break;
 		case 'v':
 			fprintf(stdout, SFM_VERSION);
-			exit(0);
 			break;
+		default:
+                        fprintf(stdout,
+			  "usage: %s [-n ncurses|-g graphical|-v version]\n",
+                           argv[0]);
+			return 1;
 		}
-		user++;
 	}
-	
-	if (!user)
-		fprintf(stdout, "usage: %s [-n ncurses|-g graphical|-v version]\n", argv[0]);
 
 	return 0;
 }
